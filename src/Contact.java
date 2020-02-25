55dcd46fce8291dc015a69e849c723a4b95eb312
gouz7514 <gouz7514@gmail.com>
2020-02-25 15:58:03 +0900

    tutorial-2:create getter and setter

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,24 @@
 public class Contact {
-    String name;
-    String email;
-    String phoneNumber;
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
+public void setName(String name) {
+this.name = name;
+}
+public void setEmail(String email) {
+this.email = email;
+}
+public void setPhoneNumber(String phoneNumber) {
+this.phoneNumber = phoneNumber;
+}
+
+public String getName() {
+return name;
+}
+public String getEmail() {
+return email;
+}
+public String getPhoneNumber() {
+this.phoneNumber = phoneNumber;
 }

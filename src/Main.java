7abdd1748685d880b0ff561f414a8142ba58a7a6
Prakash330 <72188375+Prakash330@users.noreply.github.com>
2020-10-03 00:58:34 +0530

    Modify the main class
    change the class name and improve the code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    private List<Contact> contacts = new ArrayList<>();
+public class GetContactDetails {
+    private List<Contact> contacts = new ArrayList();
 
     public static void main(String[] args) {
         Main myContact = new Main();

de3a7933f6c5f277f465e78c9a4c623f4174211e
shinjunseok <sjsage522@gmail.com>
2021-10-11 11:03:35 +0900

    tutorial-23: Merge Conflict 해결하기 Author: sjsage522

--- src/Contact.java
+++ src/Contact.java
@@ -2,4 +2,28 @@ public class Contact {
     private String name;
     private String email;
     private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
